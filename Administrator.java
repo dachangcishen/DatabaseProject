84423b8d1a6caf11548a9c9cbd8d43823f599607
BAI Yuan <89905429+byydzh@users.noreply.github.com>
2023-12-01 15:25:28 +0800

    Add files via upload

--- Administrator.java
+++ Administrator.java
@@ -2,53 +2,13 @@ import java.sql.*;
 import java.util.*;
 import java.io.*;
 public class Administrator {
-    public static void oldcreateAllTables(Connection con) throws SQLException{
-        Statement stmt = con.createStatement();
-        stmt.executeUpdate("CREATE TABLE category ("
-                + "cid INT NOT NULL,"
-                + "cname VARCHAR(20) NOT NULL,"
-                + "PRIMARY KEY (cid)"
-                + ");");
-        stmt.executeUpdate("CREATE TABLE manufacturer ("
-                + "mid INT NOT NULL,"
-                + "mname VARCHAR(20) NOT NULL,"
-                + "maddress VARCHAR(50) NOT NULL,"
-                + "mphonenumber INT NOT NULL,"
-                + "PRIMARY KEY (mid)"
-                + ");");
-        stmt.executeUpdate("CREATE TABLE part ("
-                + "pid INT NOT NULL,"
-                + "pname VARCHAR(20) NOT NULL,"
-                + "pprice INT NOT NULL,"
-                + "mid INT NOT NULL,"
-                + "cid INT NOT NULL,"
-                + "pwarranty INT NOT NULL,"
-                + "pavailablequantity INT NOT NULL,"
-                + "PRIMARY KEY (pid),"
-                + ");");
-        stmt.executeUpdate("CREATE TABLE salesperson ("
-                + "sid INT NOT NULL,"
-                + "sname VARCHAR(20) NOT NULL,"
-                + "saddress VARCHAR(50) NOT NULL,"
-                + "sphonenumber INT NOT NULL,"
-                + "sexperience INT NOT NULL,"
-                + "PRIMARY KEY (sid)"
-                + ");");
-        stmt.executeUpdate("CREATE TABLE transaction ("
-                + "tid INT NOT NULL,"
-                + "pid INT NOT NULL,"
-                + "sid INT NOT NULL,"
-                + "tdate DATE NOT NULL,"
-                + "PRIMARY KEY (tid),"
-                + ");");
-        stmt.close();
-    }
+
     public static void createAllTables(Connection con) throws SQLException {
         Statement stmt = con.createStatement();
-        String str = "CREATE TABLE category( " +
+                String str = "CREATE TABLE category( " +
             "cID INTEGER, " +
             "cName VARCHAR(25), " +
-            "PRIMARY KEY (cID)); ";
+            "PRIMARY KEY (cID))";
         try{
             stmt.executeUpdate(str);
         }catch(Exception e){System.out.println("Table category exist.");}
@@ -58,7 +18,7 @@ public class Administrator {
             "mName VARCHAR(25), " +
             "mPhoneNumber INTEGER, " +
             "mAddress VARCHAR(100), " +
-            "PRIMARY KEY (mID));";
+            "PRIMARY KEY (mID))";
         try{
             stmt.executeUpdate(str);
         }catch(Exception e){System.out.println("Table manufacturer exist.");}
@@ -71,9 +31,7 @@ public class Administrator {
             "cID INTEGER, " +
             "pWarrantyPeriod INTEGER, " +
             "pAvailableQuantity INTEGER, " +
-            "PRIMARY KEY (pID)," +
-            "FOREIGN KEY (mID) REFERENCES manufacturer(mID)," +
-            "FOREIGN KEY (cID) REFERENCES category(cID));";
+            "PRIMARY KEY (pID))";
         try{
             stmt.executeUpdate(str);
         }catch(Exception e){System.out.println("Table part exist.");}
@@ -84,7 +42,7 @@ public class Administrator {
             "sAddress VARCHAR(100), " +
             "sPhoneNumber INTEGER, " +
             "sExperience INTEGER, " +
-            "PRIMARY KEY (sID));";
+            "PRIMARY KEY (sID))";
         try{
             stmt.executeUpdate(str);
         }catch(Exception e){System.out.println("Table salesperson exist.");}
@@ -94,9 +52,7 @@ public class Administrator {
             "pID INTEGER, " +
             "sID INTEGER, " +
             "tDate DATE, " +
-            "PRIMARY KEY (tID)," +
-            "FOREIGN KEY (pID) REFERENCES part(pID)," +
-            "FOREIGN KEY (sID) REFERENCES salesperson(sID));";
+            "PRIMARY KEY (pID))";
         try{
             stmt.executeUpdate(str);
         }catch(Exception e){System.out.println("Table transaction exist.");}
